505020d9b349735e6425baf8c9aa498d480ccd53
Mikkel Berg Eriksen <eriksen127@msn.com>
2019-11-19 15:39:38 +0100

    Gamesequence-metode påbegyndt

--- src/dk/michaelwestergaard/controllers/GameController.java
+++ src/dk/michaelwestergaard/controllers/GameController.java
@@ -1,9 +1,15 @@
 package dk.michaelwestergaard.controllers;
 
+import java.util.Scanner;
+
 public class GameController {
 
     AIController aiController;
     BoardController boardController = new BoardController();
+    Scanner scan = new Scanner(System.in);
+
+    boolean gameInProgress, currentPlayer;
+    String input, startPosition, endPosition;
 
     public void startGame(){
         boardController.resetBoard();
@@ -11,7 +17,19 @@ public class GameController {
     }
 
     private void playGame(){
+        while(gameInProgress) {
+            if(currentPlayer) { //Players turn to play
+                System.out.println("Det er din tur til at spille! Lav et træk.");
 
+                input = scan.nextLine();
+                startPosition = input.substring(0, 1);
+                endPosition = input.substring(3);
+
+                boardController.move(startPosition, endPosition);
+            } else { //AI's turn to play
+
+            }
+        }
     }
 
 }
